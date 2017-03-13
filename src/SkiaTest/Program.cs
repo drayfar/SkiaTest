e43cd2f2a8cefc7111de46dc55c1d7514d661390
Danny Farmer <dfarmer@microsoft.com>
2017-03-13 14:50:36 -0700

    Add project files.

---
+++ src/SkiaTest/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace SkiaTest
+{
+    using System;
+    using System.Net.Http;
+
+    using SkiaSharp;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            SKBitmap bitmap;
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    var bytes = client.GetByteArrayAsync("https://s-media-cache-ak0.pinimg.com/564x/65/5e/06/655e06b1b452d8001d26fb21ad581e31.jpg");
+                    bytes.Wait();
+                    bitmap = SKBitmap.Decode(bytes.Result);
+                    Console.WriteLine($"Width: {bitmap.Width}; Height: {bitmap.Height}");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
